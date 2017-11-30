a6ea89f2406efece1888f4d33b915631c6460514
Marco Alka <me@marco-alka.de>
2017-11-30 13:46:16 +0100

    Simplify unwrapAnd()

--- src/result.unwrapAnd.c.js
+++ src/result.unwrapAnd.c.js
@@ -5,9 +5,5 @@ const sym = require('../interface/result-sym.h');
 
 
 H.unwrapAnd = function ($val) {
-    if (this[sym.isOk]) {
-        return $val;
-    }
-
-    throw new Error('The contained value is an Err!');
+    return this.and($val).unwrap();
 };
