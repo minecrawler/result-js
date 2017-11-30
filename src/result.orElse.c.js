a404336ebc1fc23be2baa0699a040e65b99c9c53
Marco Alka <me@marco-alka.de>
2017-11-30 13:34:10 +0100

    fix bug
    which leads to using an uninstantiated method instead of using the correct static method

--- src/result.orElse.c.js
+++ src/result.orElse.c.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const hProto = require('../interface/result.h').prototype;
+const h = require('../interface/result.h');
+const hProto = h.prototype;
 const sym = require('../interface/result-sym.h');
 
 
 hProto.orElse = function ($resultEmitter) {
     return this[sym.isOk]
         ? this
-        : hProto.fromTry($resultEmitter(this[sym.value]))
+        : h.fromTry($resultEmitter(this[sym.value]))
     ;
 };
