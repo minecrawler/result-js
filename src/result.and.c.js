4ad77868f62a03256889937e709edaa3882d1e4e
Marco Alka <me@marco-alka.de>
2017-11-30 14:05:57 +0100

    fix FP immutability of and()

--- src/result.and.c.js
+++ src/result.and.c.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const H = require('../interface/result.h').prototype;
+const h = require('../interface/result.h');
+const hProto = h.prototype;
 const sym = require('../interface/result-sym.h');
 
 
 H.and = function ($val) {
-    if (this[sym.isOk]) {
-        this[sym.value] = $val;
-    }
-
-    return this;
+    return this[sym.isOk]
+        ? h.fromSuccess($val)
+        : this
+    ;
 };
