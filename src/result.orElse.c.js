2b4741a435f813ae3830d805076e7088b3910142
Marco Alka <me@marco-alka.de>
2017-11-30 13:58:38 +0100

    fix bug

--- src/result.orElse.c.js
+++ src/result.orElse.c.js
@@ -8,6 +8,6 @@ const sym = require('../interface/result-sym.h');
 hProto.orElse = function ($resultEmitter) {
     return this[sym.isOk]
         ? this
-        : h.fromTry($resultEmitter(this[sym.value]))
+        : h.fromTry(() => $resultEmitter(this[sym.value]))
     ;
 };
